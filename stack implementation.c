a193e9cbe78a587cbdc189051269716ef3124534
Tushar G A <tusharganvekar1234@gmail.com>
2025-10-13 20:04:16 +0530

    stack implementation.c

--- stack implementation.c
+++ stack implementation.c
@@ -1,78 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 5
-struct stack{
-int top = -1;
-int data[SIZE];};
-typedef struct stack STACK;
-void push(STACK *s,int item)
-{
-    if(s->top==SIZE-1)
-        printf("\n stack overflow");
-    else
-    {
-        s->top=s->top+1;
-        s->data[s->top]=item;
+
+typedef struct stack {
+    int top;
+    int data[SIZE];
+} STACK;
+
+void push(STACK *s, int item) {
+    if (s->top == SIZE - 1) {
+        printf("\nStack overflow");
+    } else {
+        s->top++;
+        s->data[s->top] = item;
     }
 }
-void pop(STACK *s)
-{
-    if(s->top==-1)
-        printf("\n stack underflow");
-    else
-    {
-        printf("\n element popped is%d",s->data[s->top]);
-        s->top=s->top-1;
+
+void pop(STACK *s) {
+    if (s->top == -1) {
+        printf("\nStack underflow");
+    } else {
+        printf("\nElement popped is %d", s->data[s->top]);
+        s->top--;
     }
 }
-void display(STACK s)
-{
+
+void display(const STACK *s) {
     int i;
-    if(s.top==-1)
-        printf("\n stack is empty");
-    else
-    {
-        printf("\n stack content are\n");
-        for(i=s.top;i>=0;i--)
-            printf("%d\n",s.data[i]);
-    }
-}
-int main()
-{
-    int ch,item;
-    STACK s;
-    s.top=-1;
-    for(;;)
-    {
-        printf("\n 1.push");
-        printf("\n 2.pop");
-        printf("\n 3.display");
-        printf("\n 4.exit");
-        printf("\n Read choice");
-        scanf("%d",&ch);
-
-        switch(ch)
-        {
-        case 1: printf("\n read element to be pushed");
-        scanf("%d",&item);
-        push(&s,item);
-        break;
-        case 2: pop(&s);
-        break;
-
-        case 3: display(s);
-        break;
-
-        default:exit(0);
+    if (s->top == -1) {
+        printf("\nStack is empty");
+    } else {
+        printf("\nStack contents are:\n");
+        for (i = s->top; i >= 0; i--) {
+            printf("%d\n", s->data[i]);
         }
     }
-    return 0;
 }
 
+int main() {
+    int ch, item;
+    STACK s;
+    s.top = -1;
 
+    while (1) {
+        printf("\n\n1. Push");
+        printf("\n2. Pop");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
 
+        switch (ch) {
+            case 1:
+                printf("Enter element to be pushed: ");
+                scanf("%d", &item);
+                push(&s, item);
+                break;
+            case 2:
+                pop(&s);
+                break;
+            case 3:
+                display(&s);
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice, try again.");
+        }
+    }
 
-
-
-
+    return 0;
+}
 
